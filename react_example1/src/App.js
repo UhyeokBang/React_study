ac567e935987fb8f06ce63c3bae10f2862110e60
Bang Uhyeok <0105bang@gmail.com>
2024-02-21 19:17:32 +0900

    added Header and Footer in app.js

--- react_example1/src/App.js
+++ react_example1/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
+
 
 function App() {
   return (
     <div className="App">
+      <MyHeader />
       <header className="App-header">
         <h2>안녕 리액트</h2>
       </header>
+      <MyFooter />
     </div>
   );
 }
