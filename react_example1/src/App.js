fc0c837f79cb95b87eeaba6e7e114057b3bda23f
Bang Uhyeok <0105bang@gmail.com>
2024-02-22 22:02:27 +0900

    added Container.js and OddEvenResult.js on example file and modified such few others

--- react_example1/src/App.js
+++ react_example1/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Counter from "./Counter";
+import Container from "./Container";
 
 function App() {
+  const CounterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+  };
   return (
-    <div>
-      <Counter />
-    </div>
+    <Container>
+      <div>
+        <Counter {...CounterProps} />
+      </div>
+    </Container>
   );
 }
 
