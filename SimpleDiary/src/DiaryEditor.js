78f94ddf8d32a217280b5d8d24f8036b79837180
Bang Uhyeok <0105bang@gmail.com>
2024-02-23 18:29:40 +0900

    added onChange event handler and modified few others

--- SimpleDiary/src/DiaryEditor.js
+++ SimpleDiary/src/DiaryEditor.js
@@ -5,29 +5,27 @@ const DiaryEditor = () => {
     author: "",
     content: "",
   });
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
+          name="author"
           vlaue={state.author}
-          onChange={(e) => {
-            setState({
-              ...state,
-              author: e.target.value,
-            });
-          }}
+          onchange={handleChangeState}
         ></input>
       </div>
       <div>
         <textarea
+          name="content"
           vlaue={state.content}
-          onChange={(e) => {
-            setState({
-              ...state,
-              content: e.target.value,
-            });
-          }}
+          onChange={handleChangeState}
         />
       </div>
     </div>
