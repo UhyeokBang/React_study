1f2b73a3bd55eb64fd95930be0fb57fbcf764011
Bang Uhyeok <0105bang@gmail.com>
2024-02-23 23:26:34 +0900

    added DiaryList components - DiaryList.js, DiaryItem.js

--- SimpleDiary/src/App.js
+++ SimpleDiary/src/App.js
@@ -1,9 +1,34 @@
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
+const dummyList = [
+  {
+    id: 1,
+    author: "방우혁",
+    content: "hello 1",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "정도겸",
+    content: "hello 2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "최지우",
+    content: "hello 3",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+];
 function App() {
   return (
     <div>
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
