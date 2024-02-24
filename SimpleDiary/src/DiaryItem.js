164283d985504accae3a3177ddeb3f3165ebd3c2
Bang Uhyeok <0105bang@gmail.com>
2024-02-24 23:01:25 +0900

    added function that modifing diary list

--- SimpleDiary/src/DiaryItem.js
+++ SimpleDiary/src/DiaryItem.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const DiaryItem = ({
   author,
   content,
@@ -6,6 +7,21 @@ const DiaryItem = ({
   id,
   onDelete,
 }) => {
+  const [localContent, setLocalContent] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 삭제하시겠습니까?`)) {
+      onDelete(id);
+    }
+  };
+  const handleLocalContent = () => {
+    setLocalContent(localContent);
+  };
+  const handleEdit = () => {
+    setIsEdit(true);
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -15,16 +31,26 @@ const DiaryItem = ({
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}번째 일기를 삭제하시겠습니까?`)) {
-            onDelete(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      {isEdit ? (
+        <>
+          <textarea value={localContent} onChange={handleLocalContent} />
+        </>
+      ) : (
+        <>
+          <div className="content">{content}</div>
+        </>
+      )}
+      {isEdit ? (
+        <>
+          <button onClick={handleRemove}>수정취소</button>
+          <button onClick={handleEditDone}>수정완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRemove}>삭제하기</button>
+          <button onClick={handleEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
